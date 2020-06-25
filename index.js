0b80e9370b17a3f62b48457f0d12adb7d2c67ccb
Abigail Grundy <abigial.grundy@bbc.co.uk>
2020-06-25 13:56:33 +0100

    add while loop to only find email addresses from softwire

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var fs = require("fs");
 
 function emailExtraction() {
-  fs.readFile("emails.txt", function (err, buf) {
-    console.log(buf.toString());
+  fs.readFile("emails.txt", function (err, data) {
+    let textData = data.toString();
+    let counter = 0;
+
+    while (textData.search("@softwire.com") >= 0) {
+      const firstInstance = textData.search("@softwire.com");
+      counter++;
+      textData = textData.slice(firstInstance + 1);
+    }
+    console.log(counter);
   });
 }
-
 emailExtraction();
